734e0a3c22b52875bab229d8a4f17ee71844014e
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-07-18 11:46:00 +0530

    section 2cxompleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      backgroundImage: {
+        'Conclusion': "url('./images/webp/background-img.webp')",
+      },
     },
   },
   plugins: [],
